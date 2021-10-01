a6e12515c2e9fb16c1f55f3a5c5c22282f6bb5f5
Steven Masfaraud <masfaraud@dessia.tech>
2021-10-01 19:52:40 +0200

    WIP: Pulls

--- git_project_management/models.py
+++ git_project_management/models.py
@@ -33,6 +33,7 @@ class GiteaPlatform(GitPlatform):
                            params={'access_token': self.token})
         # print(req.text, req.url)
         for req_issue in req.json():
+            # print('req_issue', req_issue)
             owner_name = req_issue['owner']['username']
             repo = GiteaRepository.get(platform=self,
                                        name=req_issue['name'],
@@ -43,6 +44,9 @@ class GiteaPlatform(GitPlatform):
                                        name=req_issue['name'],
                                        owner=owner_name
                               )
+            else:
+                repo.name = req_issue['name']
+                repo.owner = owner_name
     
     @pony.orm.db_session()
     def get_issues(self):
@@ -66,11 +70,11 @@ class GiteaRepository(Repository):
         for req_issue in req.json():
             issue = Issue.get(repository=self,
                               number=req_issue['number'])
+            created_at = int(ciso8601.parse_datetime(req_issue['created_at']).timestamp())
+            updated_at = int(ciso8601.parse_datetime(req_issue['updated_at']).timestamp())
+            closed = req_issue['state']=='closed'
+            
             if not issue:
-                created_at = int(ciso8601.parse_datetime(req_issue['created_at']).timestamp())
-                updated_at = int(ciso8601.parse_datetime(req_issue['updated_at']).timestamp())
-                print('state', req_issue['state'])
-                closed = req_issue['state']=='closed'
                 issue = Issue(number=req_issue['number'],
                               repository=self,
                               title=req_issue['title'],
@@ -81,6 +85,38 @@ class GiteaRepository(Repository):
                               created_at=created_at,
                               updated_at=updated_at
                               )
+            else:
+                issue.title = req_issue['title']
+                issue.body = req_issue['body']
+                issue.closed = closed
+
+    @pony.orm.db_session()
+    def get_pull_requests(self):
+        req = requests.get('{}/repos/{}/{}/pulls'.format(self.platform.api_url, self.owner, self.name),
+                           params={'state':'all',
+                                   'access_token': self.platform.token})
+        for req_pr in req.json():
+            pull_request = PullRequest.get(repository=self,
+                                           number=req_pr['number'])
+            created_at = int(ciso8601.parse_datetime(req_pr['created_at']).timestamp())
+            merged_at = int(ciso8601.parse_datetime(req_pr['merged_at']).timestamp())
+            merged = req_pr['merged']=='true'
+            
+            # if not pull_request:
+            #     issue = Issue(number=req_issue['number'],
+            #                   repository=self,
+            #                   title=req_issue['title'],
+            #                   body=req_issue['body'],
+            #                   api_url=req_issue['url'],
+            #                   html_url=req_issue['html_url'],
+            #                   closed=closed,
+            #                   created_at=created_at,
+            #                   updated_at=updated_at
+            #                   )
+            # else:
+            #     issue.title = req_issue['title']
+            #     issue.body = req_issue['body']
+            #     issue.closed = closed      
     
 class Issue(pony_db.Entity):
     number = pony.orm.Required(int)
@@ -116,16 +152,33 @@ class ProjectManager:
     
     @pony.orm.db_session()
     def update(self):
+        print('global update')
         for platform in GitPlatform.select():
+            print('platform', platform)
             platform.get_repos()
         for repo in Repository.select(lambda r:r.active):
+            # print(repo)
             repo.get_issues()
     
     @pony.orm.db_session()
     def plot_issues(self, months=12):
         # start_ts = time.time() - 12*31*24*3600
         for issue in Issue.select():
-            print(issue.closed, issue.title)
+            # print(issue.closed)
+            print(issue.title)
+        
+       
+class PullRequest(pony_db.Entity):
+    number = pony.orm.Required(int)
+    repository = pony.orm.Required(Repository)
+    # title = pony.orm.Required(str, 255)
+    body = pony.orm.Optional(str, 50000)
+    api_url = pony.orm.Required(str, 255)
+    html_url = pony.orm.Required(str, 255)
+    merged = pony.orm.Required(bool, default=False)
+    mergeable = pony.orm.Required(bool, default=False)
+    created_at = pony.orm.Required(int)
+    merged_at = pony.orm.Optional(int)
         
 # class Milestone(pony_db.Entity):
     
--- scripts/issues.py
+++ scripts/issues.py
@@ -6,11 +6,13 @@ Created on Sun Sep 12 17:19:04 2021
 @author: steven
 """
 
+import pony
 import git_project_management.models as gpm_models
 import config as conf
 
 project_manager = gpm_models.ProjectManager(conf.db_host, conf.db_port,
                                      conf.db_user, conf.db_name, conf.db_password)
-
-project_manager.update()
-project_manager.plot_issues()
+with pony.orm.db_session:
+    
+    project_manager.update()
+    project_manager.plot_issues()
