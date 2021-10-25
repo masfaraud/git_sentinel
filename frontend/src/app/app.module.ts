4d444299a26813544f9935925e8dd6f9f67f31a9
Steven Masfaraud <masfaraud@dessia.tech>
2021-10-25 12:27:21 +0200

    feat: init frontend

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RepositoriesComponent } from './repositories/repositories.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    RepositoriesComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
